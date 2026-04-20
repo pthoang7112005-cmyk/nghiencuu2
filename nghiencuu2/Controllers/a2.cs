96bc706e27ec94ebbf1837ce207f17cd299aa3d6
pthoang7112005-cmyk <pthoang7112005@gmail.com>
2026-04-20 13:08:36 +0700

    a

---
+++ nghiencuu2/Controllers/a2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nghiencuu2.Controllers
+{
+    public class a2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ nghiencuu2/Controllers/hom1Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nghiencuu2.Controllers
+{
+    public class hom1Controller : Controller
+    {
+        // Action a1 sẽ tương ứng với đường dẫn /hom1/a1
+        public IActionResult a1()
+        {
+            // Lệnh này sẽ tự động tìm file a1.cshtml trong thư mục Views/hom1/
+            return View();
+        }
+    }
+}
